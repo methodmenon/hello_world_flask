c96891b3b8ec360b819fd50587939b4280399ce1
methodmenon <methodmenon@gmail.com>
2014-10-05 14:44:21 -0700

    init

---
+++ hello_world.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/hello")
+def hello_world():
+	return "Hello World!"
+
+'''
+@app.route("/hello/<name>")
+def hello_person(name):
+	return "Hello {}!".format(name.title())
+'''
+
+@app.route("/hello/<name>")
+def hello_person(name):
+	html = """
+		<h1> Hello {}! </h1>
+		<p>
+			Here's a picture of a kitten. Awww...
+		</p>
+		<img src="http://placekitten.com/200/300">
+		"""
+	return html.format(name.title())
+
+if __name__ == "__main__":
+	app.run()
